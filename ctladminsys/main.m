4bf8df4602d50c886c1a484dfd1b547162bb80ad
Yoann Gini <yoann.gini@gmail.com>
2018-11-17 00:54:29 +0100

    Initial Commit

---
+++ ctladminsys/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ctladminsys
+//
+//  Created by Yoann Gini on 17/11/2018.
+//  Copyright © 2018 Yoann Gini (Open Source Project). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
